5f3d80df2b761fe4f4c5c886c9788dd4bbad115e
bananapilota <fabiopetrosillo2@gmail.com>
2023-12-25 17:10:18 +0100

    added main prompt

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-
+    Scanner scanner = new Scanner(System.in);
+    int input;
+    do{
+      System.out.println("Develhope Student management software");
+      System.out.println("1. Create a new student");
+      System.out.println("2. List all students");
+      System.out.println("3. List a specific student");
+      System.out.println("4. Delete a student");
+      System.out.println("5. Modify a student");
+      System.out.println("0. Exit the program");
+      input = scanner.nextInt();
+    }while(input > 0);
+    scanner.close();
   }
 }
