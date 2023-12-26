63570808e7134a2dd9fc08c988bda01ce4179a1f
bananapilota <fabiopetrosillo2@gmail.com>
2023-12-26 15:53:09 +0100

    merged manually with branch: feature/Prompt

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int input;
+    do{
+      printMainPrompt();
+      input = scanner.nextInt();
+    }while(input > 0);
+    scanner.close();
+  }
 
+  public static void printMainPrompt(){
+    System.out.println("Develhope Student management software");
+    System.out.println("1. Create a new student");
+    System.out.println("2. List all students");
+    System.out.println("3. List a specific student");
+    System.out.println("4. Delete a student");
+    System.out.println("5. Modify a student");
+    System.out.println("0. Exit the program");
+    System.out.print("input: ");
   }
 }
